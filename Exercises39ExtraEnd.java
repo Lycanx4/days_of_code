bb348e2f269be17f636b73c2afb9c6991e03f710
Lycanx4 <kgmyatlwin89@gmail.com>
2024-05-11 11:03:08 -0400

    add exe 39

---
+++ Exercises39ExtraEnd.java
@@ -0,0 +1,14 @@
+public class Exercises39ExtraEnd{
+    public static void main(String[]args){
+        System.out.println(extraEnd("Hello"));
+        System.out.println(extraEnd("ab"));
+        System.out.println(extraEnd("Hi"));
+    }
+
+    static String extraEnd(String str) {
+        int lastIndex = str.length();
+        String s = str.substring(lastIndex-2, lastIndex);
+        return s+s+s;
+    }
+}
+
