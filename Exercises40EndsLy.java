9628892bf2c3b838a115d71ae84c7601e70bb983
Lycanx4 <kgmyatlwin89@gmail.com>
2024-05-11 00:08:42 -0400

    add exe 40,41,42

---
+++ Exercises40EndsLy.java
@@ -0,0 +1,22 @@
+public class Exercises40EndsLy{
+    public static void main(String[]args){
+        System.out.println(endsLy("oddly"));
+        System.out.println(endsLy("y"));
+        System.out.println(endsLy("oddy"));
+    }
+
+    static boolean endsLy(String str) {
+        int lastIndex = str.length();
+        
+        if(lastIndex < 2){
+            return false;
+        }else{
+            String s = str.substring(lastIndex-2, lastIndex);
+            if(s.equals("ly")){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
---
+++ Exercises42DoubleX.java
@@ -0,0 +1,26 @@
+public class Exercises42DoubleX{
+    public static void main(String[]args){
+        System.out.println(doubleX("axxbb"));
+        System.out.println(doubleX("axaxax"));
+        System.out.println(doubleX("xxxxx"));
+
+    }
+    static boolean doubleX(String str){
+        
+        //one way
+        // if(str.contains("xx")){
+        //     return true;
+        // }
+
+        //or another
+        for(int i = 0; i<str.length()-1; i++){
+            if(str.charAt(i)== 'x' && str.charAt(i+1)=='x'){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+}
+
