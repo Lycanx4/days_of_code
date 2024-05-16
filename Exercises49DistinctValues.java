55abb8cb0cd29e019e6c3d2d475058510fcc568c
Lycanx4 <kgmyatlwin89@gmail.com>
2024-05-16 09:43:48 -0400

    Exe 49

---
+++ Exercises49DistinctValues.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercises49DistinctValues{
+    public static void main(String[]args){
+        List<String> strs1 = Arrays.asList("red", "yellow", "green", "yellow", "blue", "green", "purple");
+        List<String> strs2 = Arrays.asList("jingle", "bells", "jingle", "bells", "jingle", "all", "the", "way");
+        
+        System.out.println(distinctValues(strs1));
+        System.out.println(distinctValues(strs2));
+
+    }
+
+    public static List<String> distinctValues(List<String> strs){
+        List<String> result = new ArrayList<>();
+        for (String str : strs) {
+            if(!result.contains(str)){
+                result.add(str);
+            }
+            
+        }
+
+        return result;
+    }
+}
+
