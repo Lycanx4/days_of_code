a88c7782b88eee3f48f94da63dbf96bb5e09f59d
Lycanx4 <kgmyatlwin89@gmail.com>
2024-05-21 16:21:19 -0400

    Exe52

--- Exercise52MakeOutWord.java
+++ Exercise52MakeOutWord.java
@@ -1,8 +1,19 @@
+import java.util.HashMap;
+
 public class Exercise52MakeOutWord {
     public static void main(String[] args) {
-        System.out.println(makeOutWord("<<>>", "Yay"));
-        System.out.println(makeOutWord("<<>>", "WooHoo"));
-        System.out.println(makeOutWord("[[]]", "word"));
+
+        HashMap<String,String> myMap = new HashMap<>();
+        myMap.put("Yay", "<<>>");
+        myMap.put("WooHoo", "<<>>");
+        myMap.put("word", "[[]]");
+
+        extracted(myMap);
+
+    }
+
+    private static void extracted(HashMap<String, String> myMap) {
+        myMap.forEach((key,value)-> System.out.println(makeOutWord(value, key)));
     }
 
     public static String makeOutWord(String outWord, String word){
