06116a8b6ad4f5b5007c3c3f9101485cd8a675fa
Lycanx4 <kgmyatlwin89@gmail.com>
2024-05-22 09:21:10 -0400

    Add exe1

---
+++ Exercises1SwapEnds.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercises1SwapEnds {
+    public static void main(String[] args) {
+        List<Integer> nums1 = Arrays.asList(8,6,7,9,5);
+        swapEnds(nums1);
+        System.out.println(nums1);
+    }
+
+    public static void swapEnds(List<Integer> nums){
+        if(nums.size()==0){
+            return;
+        }
+        int temp = nums.getFirst();
+        nums.set(0, nums.getLast());
+        nums.set(nums.size()-1, temp);
+    }
+}
